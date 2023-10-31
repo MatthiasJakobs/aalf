22796dc045070bf74bf1667a3ec7cc32500f7741
Matthias Jakobs <matthias.jakobs@tu-dortmund.de>
2023-10-31 15:39:17 +0100

    try sklearn mlp

--- code/main.py
+++ code/main.py
@@ -15,6 +15,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from skorch.dataset import Dataset
 from skorch.helper import predefined_split
+from sklearn.neural_network import MLPRegressor
 
 def get_simple(L, H):
     with fixedseed(torch, 921172):
@@ -25,6 +26,7 @@ def get_simple(L, H):
 def get_complex(L, H, n_filters=32):
     with fixedseed(torch, 493817):
         model = nn.Sequential(
+
             nn.Conv1d(1, n_filters, 3, padding='same'),
             nn.BatchNorm1d(n_filters),
             nn.ReLU(),
@@ -45,33 +47,49 @@ def get_complex(L, H, n_filters=32):
     return model
 
 def main():
+    ### Australian electricity demands
     L = 48
     H = 1
     j = 0
 
-
-    ### Australian electricity demands
     X = load_monash('australian_electricity_demand')['series_value']
+    log = []
+    for i, x in enumerate(X):
+        print('-'*30, i, '-'*30)
+        log = run_experiment(log, x, L, H, j, verbose=True)
+
+    log = pd.DataFrame(log)
+    log.to_csv('results/australian_electricity_demand.csv')
+
+    ### M4 Subset
+    L = 10
+    H = 1
+    j = 0
+
+    X = load_m4_daily_bench()
     log = []
     for i, x in enumerate(X):
         print('-'*30, i, '-'*30)
         log = run_experiment(log, x, L, H, j)
 
     log = pd.DataFrame(log)
-    log.to_csv('results/australian_electricity_demand.csv')
-
-    ### M4 Subset
-    X = load_m4_daily_bench()
-    log = []
-    for i, x in enumerate(X[:50]):
-        print('-'*30, i, '-'*30)
-        log = run_experiment(log, x, L, H, j)
-
-    log = pd.DataFrame(log)
     log.to_csv('results/m4.csv')
 
+    ### KDD Cup
+    L = 24
+    H = 1
+    j = 0
 
-def run_experiment(log, X, L, H, j, verbose=False):
+    X = load_monash('kdd_cup_nomissing')['series_value']
+    log = []
+    for i, x in enumerate(X):
+        print('-'*30, i, '-'*30)
+        log = run_experiment(log, x, L, H, j, lr=2e-4, verbose=True)
+
+    log = pd.DataFrame(log)
+    log.to_csv('results/kdd.csv')
+
+def run_experiment(log, X, L, H, j, lr=1e-3, verbose=False):
     print('ts length', X.shape)
 
     # Split and normalize data
@@ -116,20 +134,25 @@ def run_experiment(log, X, L, H, j, verbose=False):
     loss_i = mean_squared_error(preds_i, y_val.squeeze())
 
     val_ds = Dataset(x_val, y_val)
-    complex_hp = {
-        'random_state': 192937,
-        'max_epochs': 5000,
-        #'max_epochs': 2000,
-        'device': None,
-        'lr': 1e-3,
-        'batch_size': 1024,
-        'callbacks': [skorch.callbacks.EarlyStopping(load_best=True)],
-        'train_split': predefined_split(val_ds)
-    }
+    #complex_hp = {
+    #    'random_state': 192937,
+    #    'max_epochs': 5000,
+    #    #'max_epochs': 2000,
+    #    'device': None,
+    #    'lr': lr,
+    #    'batch_size': 1024,
+    #    'callbacks': [skorch.callbacks.EarlyStopping(load_best=True)],
+    #    'train_split': predefined_split(val_ds)
+    #}
 
-    f_c = NeuralNetRegressor(get_complex(L, H), verbose=verbose, **complex_hp)
-    f_c.fit(x_train, y_train)
-    preds_c = f_c.predict(x_val).squeeze()
+    # f_c = NeuralNetRegressor(get_complex(L, H), verbose=verbose, **complex_hp)
+    # f_c.fit(x_train, y_train)
+    # preds_c = f_c.predict(x_val).squeeze()
+    # loss_c = mean_squared_error(preds_c, y_val.squeeze())
+
+    f_c = MLPRegressor((28,), random_state=1282781)
+    f_c.fit(x_train.squeeze(), y_train.squeeze())
+    preds_c = f_c.predict(x_val.squeeze()).squeeze()
     loss_c = mean_squared_error(preds_c, y_val.squeeze())
 
     loss_lv = mean_squared_error(x_val[..., -1:].repeat(H, 2).squeeze(), y_val.squeeze())
@@ -143,8 +166,12 @@ def run_experiment(log, X, L, H, j, verbose=False):
 
     preds_i = f_i.predict(x_test.squeeze()).squeeze()
     loss_i = mean_squared_error(preds_i, y_test.squeeze())
-    preds_c = f_c.predict(x_test).squeeze()
+
+    # preds_c = f_c.predict(x_test).squeeze()
+    # loss_c = mean_squared_error(preds_c, y_test.squeeze())
+    preds_c = f_c.predict(x_test.squeeze()).squeeze()
     loss_c = mean_squared_error(preds_c, y_test.squeeze())
+
     loss_lv = mean_squared_error(x_test[..., -1:].repeat(H, 2).squeeze(), y_test.squeeze())
     loss_mean = mean_squared_error(x_test.mean(axis=-1).reshape(-1, 1).repeat(H, 1).squeeze(), y_test.squeeze()) 
 
