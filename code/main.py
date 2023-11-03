6d527d5650ed8964081a1a52a7bc3cf98b372d0d
Matthias Jakobs <matthias.jakobs@tu-dortmund.de>
2023-11-03 10:06:34 +0100

    different notion of horizon added

--- code/main.py
+++ code/main.py
@@ -16,6 +16,7 @@ from sklearn.metrics import mean_squared_error
 from skorch.dataset import Dataset
 from skorch.helper import predefined_split
 from sklearn.neural_network import MLPRegressor
+from sklearn.ensemble import RandomForestRegressor
 
 def get_simple(L, H):
     with fixedseed(torch, 921172):
@@ -48,45 +49,52 @@ def get_complex(L, H, n_filters=32):
 
 def main():
     ### Australian electricity demands
-    L = 48
-    H = 1
+    L = 10
     j = 0
 
-    X = load_monash('australian_electricity_demand')['series_value']
+    X, horizons = load_monash('australian_electricity_demand', return_horizon=True)
+    X = X['series_value']
+    H = horizons[0]
     log = []
     for i, x in enumerate(X):
         print('-'*30, i, '-'*30)
         log = run_experiment(log, x, L, H, j, verbose=True)
 
     log = pd.DataFrame(log)
+    log.index.rename('dataset_names', inplace=True)
+    print(log)
     log.to_csv('results/australian_electricity_demand.csv')
+
 
     ### M4 Subset
     L = 10
     H = 1
     j = 0
 
-    X = load_m4_daily_bench()
+    X, horizons = load_m4_daily_bench(return_horizon=True)
     log = []
     for i, x in enumerate(X):
         print('-'*30, i, '-'*30)
-        log = run_experiment(log, x, L, H, j)
+        log = run_experiment(log, x, L, horizons[i], j)
 
     log = pd.DataFrame(log)
+    log.index.rename('dataset_names', inplace=True)
     log.to_csv('results/m4.csv')
 
     ### KDD Cup
-    L = 24
+    L = 10
     H = 1
     j = 0
 
-    X = load_monash('kdd_cup_nomissing')['series_value']
+    X, horizons = load_monash('kdd_cup_nomissing', return_horizon=True)
+    X = X['series_value']
     log = []
     for i, x in enumerate(X):
         print('-'*30, i, '-'*30)
-        log = run_experiment(log, x, L, H, j, lr=2e-4, verbose=True)
+        log = run_experiment(log, x, L, horizons[i], j, lr=2e-4, verbose=True)
 
     log = pd.DataFrame(log)
+    log.index.rename('dataset_names', inplace=True)
     log.to_csv('results/kdd.csv')
 
 def run_experiment(log, X, L, H, j, lr=1e-3, verbose=False):
@@ -102,86 +110,82 @@ def run_experiment(log, X, L, H, j, lr=1e-3, verbose=False):
     mu = np.mean(X_train)
     std = np.std(X_train)
 
-    X_train = (X_train - mu) / std
-    X_val = (X_val - mu) / std
-    X_test = (X_test - mu) / std
+    X = (X - mu) / std
+
+    X_train = X[:end_train]
+    X_val = X[end_train:end_val]
+    X_test = X[end_val:]
 
     # Instead of forecasting t+1, forecast t+j
-    x_train, y_train = windowing(X_train, L=L, H=H+j)
-    x_val, y_val = windowing(X_val, L=L, H=H+j)
-    x_test, y_test = windowing(X_test, L=L, H=H+j)
-    y_train = y_train[..., j:]
-    y_val = y_val[..., j:]
-    y_test = y_test[..., j:]
+    x_train, y_train = windowing(X_train, L=L, H=H)
+    x_val, y_val = windowing(X_val, L=L, H=H)
+    x_test, y_test = windowing(X_test, L=L, H=H)
+    y_train = y_train[..., -1:]
+    y_val = y_val[..., -1:]
+    y_test = y_test[..., -1:]
+    print(x_train.shape, y_train.shape)
+    print(x_val.shape, y_val.shape)
+    print(x_test.shape, y_test.shape)
 
     x_train = np.expand_dims(x_train.astype(np.float32), 1)
     x_val = np.expand_dims(x_val.astype(np.float32), 1)
     x_test = np.expand_dims(x_test.astype(np.float32), 1)
-    y_train = y_train.reshape(-1, H).astype(np.float32)
-    y_val = y_val.reshape(-1, H).astype(np.float32)
-    y_test = y_test.reshape(-1, H).astype(np.float32)
+    y_train = y_train.reshape(-1, 1).astype(np.float32)
+    y_val = y_val.reshape(-1, 1).astype(np.float32)
+    y_test = y_test.reshape(-1, 1).astype(np.float32)
 
     # Train base models
-    print('train shape', x_train.shape, y_train.shape)
-    print('val shape', x_val.shape, y_val.shape)
-    print('test shape', x_test.shape, y_test.shape)
+    val_ds = Dataset(x_val, y_val)
 
     f_i = LinearRegression()
     f_i.fit(x_train.squeeze(), y_train)
-    # f_i = NeuralNetRegressor(get_simple(L, H), verbose=verbose, **simple_hp)
-    # f_i.fit(x_train.squeeze(), y_train)
-    preds_i = f_i.predict(x_val.squeeze()).squeeze()
-    loss_i = mean_squared_error(preds_i, y_val.squeeze())
-
-    val_ds = Dataset(x_val, y_val)
-    #complex_hp = {
-    #    'random_state': 192937,
-    #    'max_epochs': 5000,
-    #    #'max_epochs': 2000,
-    #    'device': None,
-    #    'lr': lr,
-    #    'batch_size': 1024,
-    #    'callbacks': [skorch.callbacks.EarlyStopping(load_best=True)],
-    #    'train_split': predefined_split(val_ds)
-    #}
-
-    # f_c = NeuralNetRegressor(get_complex(L, H), verbose=verbose, **complex_hp)
-    # f_c.fit(x_train, y_train)
-    # preds_c = f_c.predict(x_val).squeeze()
-    # loss_c = mean_squared_error(preds_c, y_val.squeeze())
-
-    f_c = MLPRegressor((28,), random_state=1282781)
-    f_c.fit(x_train.squeeze(), y_train.squeeze())
-    preds_c = f_c.predict(x_val.squeeze()).squeeze()
-    loss_c = mean_squared_error(preds_c, y_val.squeeze())
-
-    loss_lv = mean_squared_error(x_val[..., -1:].repeat(H, 2).squeeze(), y_val.squeeze())
-    loss_mean = mean_squared_error(x_val.mean(axis=-1).reshape(-1, 1).repeat(H, 1).squeeze(), y_val.squeeze()) 
-
-    print('--- validation ---')
-    print('last value', loss_lv)
-    print('mean value', loss_mean)
-    print('linear', loss_i)
-    print('neural net', loss_c)
-
     preds_i = f_i.predict(x_test.squeeze()).squeeze()
     loss_i = mean_squared_error(preds_i, y_test.squeeze())
 
-    # preds_c = f_c.predict(x_test).squeeze()
-    # loss_c = mean_squared_error(preds_c, y_test.squeeze())
+
+    # Random Forests
+    f_c = RandomForestRegressor(n_estimators=16, random_state=129281, n_jobs=-1)
+    f_c.fit(x_train.squeeze(), y_train.squeeze())
     preds_c = f_c.predict(x_test.squeeze()).squeeze()
-    loss_c = mean_squared_error(preds_c, y_test.squeeze())
+    loss_c_16 = mean_squared_error(preds_c, y_test.squeeze())
 
-    loss_lv = mean_squared_error(x_test[..., -1:].repeat(H, 2).squeeze(), y_test.squeeze())
-    loss_mean = mean_squared_error(x_test.mean(axis=-1).reshape(-1, 1).repeat(H, 1).squeeze(), y_test.squeeze()) 
+    f_c = RandomForestRegressor(n_estimators=32, random_state=129281, n_jobs=-1)
+    f_c.fit(x_train.squeeze(), y_train.squeeze())
+    preds_c = f_c.predict(x_test.squeeze()).squeeze()
+    loss_c_32 = mean_squared_error(preds_c, y_test.squeeze())
 
-    print('--- test ---')
-    print('last value', loss_lv)
-    print('mean value', loss_mean)
-    print('linear', loss_i)
-    print('neural net', loss_c)
+    f_c = RandomForestRegressor(n_estimators=64, random_state=129281, n_jobs=-1)
+    f_c.fit(x_train.squeeze(), y_train.squeeze())
+    preds_c = f_c.predict(x_test.squeeze()).squeeze()
+    loss_c_64 = mean_squared_error(preds_c, y_test.squeeze())
 
-    log.append({'lv_test': loss_lv, 'mean_test': loss_mean, 'linear_test': loss_i, 'complex_test': loss_c})
+    f_c = RandomForestRegressor(n_estimators=128, random_state=129281, n_jobs=-1)
+    f_c.fit(x_train.squeeze(), y_train.squeeze())
+    preds_c = f_c.predict(x_test.squeeze()).squeeze()
+    loss_c_128 = mean_squared_error(preds_c, y_test.squeeze())
+
+    '''
+
+    f_c = RandomForestRegressor(n_estimators=256, random_state=129281, n_jobs=-1)
+    f_c.fit(x_train.squeeze(), y_train.squeeze())
+    preds_c = f_c.predict(x_test.squeeze()).squeeze()
+    loss_c_256 = mean_squared_error(preds_c, y_test.squeeze())
+
+    f_c = RandomForestRegressor(n_estimators=512, random_state=129281, n_jobs=-1)
+    f_c.fit(x_train.squeeze(), y_train.squeeze())
+    preds_c = f_c.predict(x_test.squeeze()).squeeze()
+    loss_c_512 = mean_squared_error(preds_c, y_test.squeeze())
+
+    f_c = RandomForestRegressor(n_estimators=1024, random_state=129281, n_jobs=-1)
+    f_c.fit(x_train.squeeze(), y_train.squeeze())
+    preds_c = f_c.predict(x_test.squeeze()).squeeze()
+    loss_c_1024 = mean_squared_error(preds_c, y_test.squeeze())
+    '''
+
+    loss_lv = mean_squared_error(x_test[..., -1:].squeeze(), y_test.squeeze())
+    loss_mean = mean_squared_error(x_test.mean(axis=-1).reshape(-1, 1).squeeze(), y_test.squeeze()) 
+
+    log.append({'lv_test': loss_lv, 'mean_test': loss_mean, 'linear_test': loss_i, 'complex_test_16': loss_c_16, 'complex_test_32': loss_c_32, 'complex_test_64': loss_c_64, 'complex_test_128': loss_c_128})
 
     return log
 
